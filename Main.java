0e8ceaecf3e5dc7f505f73d08ff3d311e828e267
Lidiane <130933609+lid1an3@users.noreply.github.com>
2024-04-25 15:22:55 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main{
+  public static void main(String[] args){
+    
+  String nome = "Lidiane";
+  String cor = "Castanho";
+  String idade = "17";
+  String altura = "1,65";
+
+  System.out.println("Seu nome:" + nome);
+  System.out.println("Sua cor de cabelo: " + cor);
+  System.out.println("Sua cor de olhos: " + cor);
+  System.out.println("Sua idade: " + idade);
+  System.out.println("Sua altura: " + altura);
+  }
+}
